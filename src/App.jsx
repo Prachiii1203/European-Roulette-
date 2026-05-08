58579e3273ca2a10bba2c11cf94b9d235077222e
Prachi <prachi017012@gmail.com>
2026-05-08 14:32:37 +0530

    money deduction fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import "./App.css";
 
 function App() {
   const [totalAmt, setTotalAmt] = useState(1000);
@@ -18,7 +17,6 @@ function App() {
   const [selectedBet, setSelectedBet] = useState("");
   const [msg, setMsg] = useState("");
   const [singlebet, setSinglebet] = useState(null);
-
   const [UserChipAmt, SetUserChipAmt] = useState(null);
 
   for (let i = 0; i <= 36; i++) {
@@ -26,62 +24,57 @@ function App() {
   }
 
   const WheelResult = () => {
-    if (selectedBet === "") {
-      return alert("please select bet type first");
-    }
-
     if (UserChipAmt === null) {
-      return alert("Please Select the Amount of bet");
+      return alert("Please Select The Amount of Bet");
     }
 
+    if (selectedBet === "") {
+      return alert("Please Select Bet Type First");
+    }
     let val = Math.floor(Math.random() * wheelNumbers.length);
     setSpinResult(val);
 
     switch (selectedBet) {
       case "Odd":
-        setMsg(val % 2 !== 0 ? "Win" : "Loss");
-        countBetAmt();
+        countBetAmt(val !== 0 && val % 2 !== 0);
         break;
       case "Even":
-        setMsg(val % 2 !== 0 ? "Loss" : "Win");
-        countBetAmt();
+        countBetAmt(val !== 0 && val % 2 === 0);
         break;
       case "1st 12":
-        setMsg(val > 0 && val <= 12 ? "Win" : "Loss");
-        countBetAmt(3);
+        countBetAmt(val > 0 && val <= 12, 3);
         break;
       case "2nd 12":
-        setMsg(val > 12 && val <= 24 ? "Win" : "Loss");
-        countBetAmt(3);
+        countBetAmt(val > 12 && val <= 24, 3);
         break;
       case "3rd 12":
-        setMsg(val > 24 && val <= 36 ? "Win" : "Loss");
-        countBetAmt(3);
+        countBetAmt(val > 24 && val <= 36, 3);
         break;
       case "1-18/Lower":
-        setMsg(val > 0 && val <= 18 ? "Win" : "Loss");
-        countBetAmt();
+        countBetAmt(val > 0 && val <= 18);
         break;
       case "19-36/Higer":
-        setMsg(val > 18 && val <= 36 ? "Win" : "Loss");
-        countBetAmt();
+        countBetAmt(val > 18 && val <= 36);
         break;
       case "single Value":
-        setMsg(singlebet === Number(val) ? "Win" : "Loss");
-        countBetAmt(35);
+        countBetAmt(singlebet === Number(val), 35);
         setSinglebet(null);
         break;
       default:
-        console.log("CHALA JA ");
+        // console.log("There is not such kind of Bet here");
+        return;
     }
   };
 
-  const countBetAmt = (n = 2) => {
+  const countBetAmt = (resultcondition, n = 2) => {
     if (totalAmt < UserChipAmt) {
       return alert("You don't have sufficient balance");
     }
 
-    if (msg === "Win") {
+    const resultMsg = resultcondition ? "Win" : "Loss";
+    setMsg(resultMsg);
+
+    if (resultMsg === "Win") {
       const winprice = n * UserChipAmt;
       setTotalAmt((tot) => tot + winprice);
     } else {
@@ -94,6 +87,14 @@ function App() {
     console.log(e.target.value);
   };
 
+  const clrVal = () => {
+    setSpinResult(null);
+    setMsg("");
+    setSelectedBet("");
+    setSinglebet(null);
+    SetUserChipAmt(null);
+  };
+
   return (
     <>
       <div>
@@ -102,66 +103,51 @@ function App() {
         <h3>Total Balance : {totalAmt} ₹</h3>
         <div className="divChip">
           {chipAmt.map((chip, index) => (
-            <>
-              <button
-                key={index}
-                onClick={(e) => {
-                  SetUserChipAmt(e.target.value);
-                  console.log(e.target.value);
-                }}
-                value={chip}
-              >
-                {chip} ₹
-              </button>
-            </>
+            <button
+              key={index}
+              onClick={(e) => {
+                SetUserChipAmt(Number(e.target.value));
+              }}
+              value={chip}
+            >
+              {chip} ₹
+            </button>
           ))}
         </div>
         <hr />
         <div className="divbtn">
           {wheelNumbers.map((wno, index) => (
-            <>
-              <button
-                key={index}
-                onClick={() => {
-                  setSelectedBet("single Value");
-                  setSinglebet(wno);
-                  console.log(wno);
-                  console.log("single Value");
-                }}
-              >
-                {wno}
-              </button>
-            </>
+            <button
+              key={index}
+              onClick={() => {
+                setSelectedBet("single Value");
+                setSinglebet(wno);
+              }}
+            >
+              {wno}
+            </button>
           ))}
         </div>
         <div className="betTypes">
           {Bet_type.map((bet_ty, index) => (
-            <>
-              <button key={index} value={bet_ty} onClick={SelectBetType}>
-                {bet_ty}
-              </button>
-            </>
+            <button key={index} value={bet_ty} onClick={SelectBetType}>
+              {bet_ty}
+            </button>
           ))}
         </div>
-        <button onClick={WheelResult}>Spin that shiii</button>
-        <button
-          onClick={() => {
-            setSpinResult(null);
-            setMsg("");
-            setSelectedBet("");
-          }}
-        >
-          Clear
+        <button onClick={WheelResult} className="spinbtn">
+          Spin
         </button>
+        <button onClick={clrVal}>Clear</button>
         <br />
-        <p>
-          {selectedBet && (
-            <>
-              Bet on {selectedBet} {UserChipAmt && <>of {UserChipAmt} ₹</>}{" "}
-            </>
-          )}
-          {singlebet && <p>Single Bet value : {singlebet}</p>}
-        </p>
+        {selectedBet && (
+          <p>
+            Bet on {selectedBet} {UserChipAmt && <>of {UserChipAmt} ₹</>}{" "}
+          </p>
+        )}
+        {selectedBet === "single Value" && singlebet !== null && (
+          <p>Single Bet value : {singlebet}</p>
+        )}
         {spinResult !== null && <p>Result : {spinResult}</p>}
         {msg && <p className="spinresStatus">{msg}</p>}
       </div>
