efe2931a9c2b76291ed6932ee1469fe673fd935c
Prachi <prachi017012@gmail.com>
2026-05-12 12:52:37 +0530

    Multiple bet added

--- src/App.jsx
+++ src/App.jsx
@@ -23,86 +23,127 @@ function App() {
   const [msg, setMsg] = useState("");
   const [singlebet, setSinglebet] = useState(null);
   const [UserChipAmt, SetUserChipAmt] = useState(null);
+  const [allbet, setAllBet] = useState([]);
+  const [spinbalanceFlag, setSpinbalanceFlag] = useState(false);
 
+  //to create bet numbers
   for (let i = 1; i <= 36; i++) {
     wheelNumbers.push(i);
   }
 
+  //on spin
   const WheelResult = () => {
-    if (UserChipAmt === null) {
-      return alert("Please Select The Amount To Bet");
+    //counted total amount of betted chip
+    const totalchipval = allbet.reduce((tot, val) => val.chipAmt + tot, 0);
+
+    //checking condition if spin is done and balance is 0 so it don't spin again
+    //if user have balance then cut amount of chip from it and then use proceed further
+    if (
+      spinbalanceFlag === true &&
+      (totalAmt <= 0 || totalAmt < totalchipval)
+    ) {
+      alert("you don't have sufficient balance,Please Add balance");
+      return;
+    } else if (spinbalanceFlag === true) {
+      setTotalAmt((tot) => tot - totalchipval);
+      setSpinbalanceFlag(false);
     }
 
-    if (selectedBet === "") {
-      return alert("Please Select Bet Type");
-    }
-    if (totalAmt < UserChipAmt) {
-      return alert("You don't have sufficient balance");
-    }
-
-    let val = Math.floor(Math.random() * (wheelNumbers.length + 1));
+    //spin result for random number from 0 to 36
+    let val = Math.floor(Math.random() * 37);
     setSpinResult(val);
 
-    switch (selectedBet) {
-      case "Red":
-        countBetAmt(RED_NUM.includes(val));
-        break;
-      case "Black":
-        countBetAmt(!RED_NUM.includes(val) && val !== 0);
-        break;
-      case "Odd":
-        countBetAmt(val !== 0 && val % 2 !== 0);
-        break;
-      case "Even":
-        countBetAmt(val !== 0 && val % 2 === 0);
-        break;
-      case "1st 12":
-        countBetAmt(val > 0 && val <= 12, 3);
-        break;
-      case "2nd 12":
-        countBetAmt(val > 12 && val <= 24, 3);
-        break;
-      case "3rd 12":
-        countBetAmt(val > 24 && val <= 36, 3);
-        break;
-      case "1-18/Lower":
-        countBetAmt(val > 0 && val <= 18);
-        break;
-      case "19-36/Higer":
-        countBetAmt(val > 18 && val <= 36);
-        break;
-      case "single Bet":
-        countBetAmt(singlebet === Number(val), 36);
-        // setSinglebet(null);
-        break;
-      default:
-        // console.log("There is not such kind of Bet here");
-        return;
-    }
+    //updatebet - to check bettype and return result accroding to it
+    //will check all the bets from allbet
+    const updatedBet = allbet.map((betval) => {
+      switch (betval.betType) {
+        case "Red":
+          return countBetAmt(RED_NUM.includes(val), betval);
+        case "Black":
+          return countBetAmt(!RED_NUM.includes(val) && val !== 0, betval);
+        case "Odd":
+          return countBetAmt(val !== 0 && val % 2 !== 0, betval);
+        case "Even":
+          return countBetAmt(val !== 0 && val % 2 === 0, betval);
+        case "1st 12":
+          return countBetAmt(val > 0 && val <= 12, betval, 3);
+        case "2nd 12":
+          return countBetAmt(val > 12 && val <= 24, betval, 3);
+        case "3rd 12":
+          return countBetAmt(val > 24 && val <= 36, betval, 3);
+        case "1-18/Lower":
+          return countBetAmt(val > 0 && val <= 18, betval);
+        case "19-36/Higer":
+          return countBetAmt(val > 18 && val <= 36, betval);
+        case "single Bet":
+          return countBetAmt(betval.singlebetVal === Number(val), betval, 36);
+        default:
+          alert("There is not such kind of Bet here");
+          return;
+      }
+    });
+    //setting the bet
+    setAllBet(updatedBet);
+    //to print msg of win if any one is win it is still win
+    const chkwin = updatedBet.find((betwin) => betwin.status === "Win");
+    setMsg(chkwin ? "Win" : "Loss");
+    setSpinbalanceFlag(true); //flag
   };
 
-  const countBetAmt = (resultcondition, n = 2) => {
+  //to calculate bet amount and set status
+  const countBetAmt = (resultcondition, bet, n = 2) => {
     const resultMsg = resultcondition ? "Win" : "Loss";
-    setMsg(resultMsg);
-
+    let winprice;
     if (resultMsg === "Win") {
-      const winprice = n * UserChipAmt;
-      setTotalAmt((tot) => tot + winprice - UserChipAmt);
-    } else {
-      setTotalAmt((tot) => tot - UserChipAmt);
+      winprice = n * bet.chipAmt;
+      setTotalAmt((tot) => tot + winprice);
     }
+
+    return {
+      ...bet,
+      ["amt"]: resultMsg === "Win" ? winprice : bet.chipAmt,
+      ["status"]: resultMsg,
+    };
   };
 
+  //to select bet type and set in selectedbet
   const SelectBetType = (e) => {
     setSelectedBet(e.target.value);
-    console.log(e.target.value);
+    addMultipleBet(e.target.value, UserChipAmt);
   };
 
+  //to clear all bet and result
   const clrVal = () => {
     setSpinResult(null);
     setMsg("");
+    setAllBet([]);
+    setSpinbalanceFlag(false);
+  };
+
+  //to create a object for multiple bet
+  const addMultipleBet = (bet, chip) => {
+    //check if the chip value is not greater than total balance
+    if (totalAmt < chip) {
+      alert("You don't have sufficient balance");
+      return;
+    }
+
+    //according to bet data is set in allbet array
+    let res;
+    if (bet === "single Bet" && singlebet !== null && chip !== null) {
+      res = { betType: bet, chipAmt: chip, singlebetVal: singlebet };
+      setSinglebet(null);
+    } else if (bet !== "" && chip !== null) {
+      res = { betType: bet, chipAmt: chip };
+    } else {
+      return;
+    }
+
+    //cut chip amount from total
+    setTotalAmt((tot) => tot - chip);
+    setAllBet((allprev) => [...allprev, res]);
+
     setSelectedBet("");
-    setSinglebet(null);
     SetUserChipAmt(null);
   };
 
@@ -111,7 +152,7 @@ function App() {
       <div>
         <h1>European Roulette Casino</h1>
         <h3>Total Chip Balance : {totalAmt} ₹</h3>
-        <div className="chipInfo">
+        <section className="chipInfo">
           <div className="divChip">
             {chipAmt.map((chip, index) => (
               <button
@@ -119,6 +160,7 @@ function App() {
                 key={index}
                 onClick={(e) => {
                   SetUserChipAmt(Number(e.target.value));
+                  addMultipleBet(selectedBet, Number(e.target.value));
                 }}
                 value={chip}
               >
@@ -127,6 +169,7 @@ function App() {
             ))}
             <br />
             <button
+              className="add1kbtn"
               onClick={() => {
                 if (window.confirm("You sure want to add 1000 ?")) {
                   setTotalAmt((t) => t + 1000);
@@ -138,21 +181,29 @@ function App() {
             </button>
           </div>
           <div className="chipData">
-            {selectedBet && <p>Bet Type : {selectedBet} </p>}
-            {selectedBet === "single Bet" && singlebet !== null && (
-              <p>Single Bet on : {singlebet}</p>
-            )}
-            {UserChipAmt && <p>Chip : {UserChipAmt} ₹</p>}
+            {allbet.map((abet, index) => (
+              <div key={index}>
+                {abet.betType && <p>Bet Type : {abet.betType} </p>}
+                {abet.betType === "single Bet" &&
+                  abet.singlebetVal !== null && (
+                    <p>Single Bet on : {abet.singlebetVal}</p>
+                  )}
+                {abet.chipAmt && <p>Chip : {abet.chipAmt} ₹</p>}
+              </div>
+            ))}
           </div>
-        </div>
+        </section>
         <hr />
-        <div className="divbtn">
+        <section className="divbtn">
           <div className="btn0">
             <button
               className="Greenbtn"
               onClick={() => {
                 setSelectedBet("single Bet");
                 setSinglebet(0);
+                if (UserChipAmt !== null) {
+                  addMultipleBet("single Bet", UserChipAmt);
+                }
               }}
             >
               0{" "}
@@ -174,7 +225,7 @@ function App() {
               </button>
             ))}
           </div>
-        </div>
+        </section>
         <div className="betTypes">
           {Bet_type.map((bet_ty, index) => (
             <button key={index} value={bet_ty} onClick={SelectBetType}>
@@ -185,7 +236,7 @@ function App() {
         <button
           onClick={WheelResult}
           className="spinbtn"
-          disabled={!selectedBet || !UserChipAmt}
+          disabled={allbet.length <= 0}
         >
           Spin
         </button>
@@ -202,6 +253,16 @@ function App() {
             </p>
           )}
         </div>
+        <section className="allbetdiv">
+          {spinResult &&
+            allbet.map((abet, index) => (
+              <p key={index}>
+                <span>
+                  {abet.betType} - You {abet.status} {abet.amt}
+                </span>
+              </p>
+            ))}
+        </section>
       </div>
     </>
   );
